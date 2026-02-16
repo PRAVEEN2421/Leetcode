7b3b3669cd0ccf07e797f4a0cccda6a96f730d8f
Sai Praveen <162142454+PRAVEEN2421@users.noreply.github.com>
2026-02-16 10:31:57 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3099-harshad-number/3099-harshad-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var sumOfTheDigitsOfHarshadNumber = function(x) {
+
+
+let sum = 0
+let temp = x
+
+ while(temp > 0){
+     sum += temp % 10
+     temp = Math.floor(temp / 10)
+ }
+  return  temp % sum === 0 ? sum : -1
+   
+};
