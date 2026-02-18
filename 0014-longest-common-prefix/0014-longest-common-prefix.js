97f290004fcc863f2a4bbe87e0bd6ca6ab049b0e
Sai Praveen <162142454+PRAVEEN2421@users.noreply.github.com>
2026-02-18 14:29:44 +0530

    Time: 4 ms (24.24%), Space: 54.8 MB (36.52%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    if(strs.length === 0 ) return ""
+
+    let prifix = ""
+
+    for(let i=0;i<strs[0].length;i++){
+        let char = strs[0][i]
+        for(let j=1;j<strs.length;j++){
+            if(strs[j][i] !== char){
+                return prifix
+            }
+        }
+
+        prifix +=char
+    }
+
+    return prifix
+
+};
